8acbce87800128aad4d01e8245007ec148cace70
Радугина Лиза <raduginaelizaveta@gmail.com>
2025-09-26 13:49:50 +0300

    Create_reader

--- gth_db.py
+++ gth_db.py
@@ -4,4 +4,13 @@ con = sqlite3.connect("Library_db.db")
 curs = con.cursor()
 #start code
 
+def create():
+    curs.execute("""CREATE TABLE IF NOT EXISTS reader (
+                 id INTEGER PRIMARY KEY
+                 pr TEXT PRIMARY KEY
+                 full_name TEXT NOT NULL
+                 phone TEXT NOT NULL
+                age INTEGER NOT NULL
+    )""")
+
 con.commit()
